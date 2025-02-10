538d4f600cc9d74242e64d6c1b82b5fdb636ccab
송채영 <dkdl0806@naver.com>
2025-02-10 16:39:57 +0900

    2/7 과제

--- homework/src/hw0207/Phone.java
+++ homework/src/hw0207/Phone.java
@@ -4,20 +4,20 @@ public class Phone {
 
 	public static void main(String[] args) {
 		
-		ContactApp contactApp = new ContactApp(10);
+		ContactApp contactApp = new ContactApp(4);
 		
 		Contact contact1 = new Contact(3, "민지", "010-1234-5678", "photo1.jpg");
 		Contact contact2 = new Contact(1, "하니", "010-2345-6789", "photo2.jpg");
 		Contact contact3 = new Contact(4, "다니엘", "010-3456-7890", "photo3.jpg");
 		Contact contact4 = new Contact(2, "혜린", "010-4567-8901", "photo4.jpg");
-
-		Contact addcontact1 = new AdditionalContact(5, "혜인", "010-5678-2345", "photo5.jpg", 20080421, "NYZ", "member","Seoul");
 		
-		contactApp.addNewContact(addcontact1);
 		contactApp.addNewContact(contact1);
 		contactApp.addNewContact(contact2);
 		contactApp.addNewContact(contact3);
 		contactApp.addNewContact(contact4);
+		
+		Contact addcontact1 = new AdditionalContact(5, "혜인", "010-5678-2345", "photo5.jpg", 20080421, "NYZ", "member","Seoul");
+		contactApp.addNewContact(addcontact1);
 
 		contactApp.bubbleSort();
 		System.out.println("Bublle Sort 후 연락처 목록");
